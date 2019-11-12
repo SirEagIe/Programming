32343a6713fa47b4e82f22f07cf8a95790988532
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-12 23:14:56 +1000

    Update 31.c

--- Крячков/Первая глава/31.c
+++ Крячков/Первая глава/31.c
@@ -10,7 +10,6 @@ int main()
     setlocale(LC_CTYPE, "rus");
     int a, b;
     float P, k;
-    int key;
     printf("Введите значения a, b\n");
     scanf("%d%d", &a, &b);
     if (a%b == 0 || a == 2)
@@ -19,9 +18,5 @@ int main()
         k = (1. + b)/(2. - a);
     P = (min(a + b, 2*a)) + k*(min(a - 3*b, 25));
     printf("P = %.2f\n", P);
-    key = getch();
-    if (key == 113)
-        break;
-    printf("\n");
     return 0;
 }

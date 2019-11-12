3da3caccca37f97c08dc2dbfafe714f878e47ad7
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-12 23:25:55 +1000

    Update 1.c

--- Павловская/Первый семинар/1.c
+++ Павловская/Первый семинар/1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main()
+{
+	float fahr;
+	printf("Enter Fahrenheit Temperature: \n");
+	scanf("%f", &fahr);
+	float cels=(fahr-32)*5/9;
+	printf("Fahrenheit: %f, Celsius: %f", fahr, cels);
+	return 0;
+}

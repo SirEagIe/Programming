2f66cf5d418c93424f0eb21822b6f09ce5302500
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-12 23:18:20 +1000

    Update 57.c

--- Крячков/Первая глава/57.c
+++ Крячков/Первая глава/57.c
@@ -7,24 +7,17 @@ int main()
 {
     setlocale(LC_CTYPE, "rus");
     int n, m;
-    int key;
-    for( ; ; )
+    double S = 0;
+    printf("Введите n, m\n");
+    scanf("%d%d", &n, &m);
+    for(int i = 1; i <= n; i++)
     {
-        double S = 0;
-        printf("¬ведите n, m\n");
-        scanf("%d%d", &n, &m);
-        for(int i = 1; i <= n; i++)
+        for(int k = 1; k <= m; k++)
         {
-            for(int k = 1; k <= m; k++)
-            {
-                S += sin((M_PI/4)*i + (M_PI/8)*pow(k, 2));
-            }
+            S += sin((M_PI/4)*i + (M_PI/8)*pow(k, 2));
         }
-        printf("–езультат равен %lf\n", S);
-        key = getch();
-        if (key == 113)
-            break;
-        printf("\n");
+    }
+    printf("Результат равен %lf\n", S);
     }
     return 0;
 }

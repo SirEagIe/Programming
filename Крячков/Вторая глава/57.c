49f4dd6604f8960ea430be6ea0e774df56a2985e
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-15 21:26:39 +1000

    Create 57.c

---
+++ Крячков/Вторая глава/57.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int temp = 0, max = 0;
+    char text[80];
+    printf("Введите текст\n");
+    gets(text);
+    for(int i = 0; i < strlen(text); i++)
+    {
+        if(text[i] != ' ')
+            temp++;
+        if(text[i] == ' ' || i == (strlen(text) - 1))
+        {
+            if(max < temp)
+                max = temp;
+            temp = 0;
+        }
+    }
+    printf("Количество символов в слове максимальной длины равно %d", max);
+    return 0;
+}

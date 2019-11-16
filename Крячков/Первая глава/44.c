e0e4b2d6b4abc51665e9776113aa54cb6bfa0015
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-16 12:00:54 +1000

    Update 44.c

--- Крячков/Первая глава/44.c
+++ Крячков/Первая глава/44.c
@@ -3,20 +3,26 @@
 #include <locale.h>
 #include <math.h>
 
-#define min(a, b) (a<b) ? a : b
-
 int main()
 {
     setlocale(LC_CTYPE, "rus");
-    int a, b;
-    float P, k;
-    printf("Введите значения a, b\n");
-    scanf("%d%d", &a, &b);
-    if (a%b == 0 || a == 2)
-        k = a*a + b;
-    else
-        k = (1. + b)/(2. - a);
-    P = (min(a + b, 2*a)) + k*(min(a - 3*b, 25));
-    printf("P = %.2f\n", P);
+    int n, f1 = 1, f2 = 1, f3 = f1 + f2, result;
+    printf("Введите n > 10\n");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        if(i == 0)
+            printf("%d", f1);
+        else
+            printf(" + %d", f1);
+        if(i == 0)
+            result = f1;
+        else
+            result += f1;
+        f1 = f2;
+        f2 = f3;
+        f3 = f1 + f2; //1+1+2+3+5+8
+    }
+    printf(" = %d", result);
     return 0;
 }

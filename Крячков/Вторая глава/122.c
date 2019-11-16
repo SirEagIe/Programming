367ef6e96350d18c448ae675f7ec8670ebb60cf5
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-11-16 12:52:44 +1000

    Create 122.c

---
+++ Крячков/Вторая глава/122.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    srand(time(NULL));
+    int N, M, znak;
+    printf("Введите числа N, M, N <= M, M < 13\n");
+    scanf("%d%d", &N, &M);
+    int B[N][M];
+    int D[M];
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+        {
+            B[i][j] = rand()%1000;
+            znak = rand();
+            if(znak % 2 == 0)
+                B[i][j] *= -1;
+        }
+    }
+    printf("Элементы матрицы равны:\n");
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < M; j++)
+            printf("%6d ", B[i][j]);
+            printf("\n");
+    }
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            if(j == 0)
+                D[i] = 0;
+            D[i] += B[j][i];
+        }
+    }
+    printf("Суммы элементов столбцов матрицы равны\n");
+    for(int i = 0; i < M; i++)
+        printf("%6d ", D[i]);
+    return 0;
+}
+/*
+     M
+   N 1 2 3
+     3 2 1
+     4 5 6
+
+     8 9 10
+     D[0] = B[0][0] + B[0][1]+ B[0][2]
+
+*/

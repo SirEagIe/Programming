ad3ee9516e43703e676e9be83b39aba3e3dd1463
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2019-12-27 14:40:52 +1000

    Create 3.c

---
+++ Павловская/Четвёртый семинар/3.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    srand(time(NULL));
+    int nrow, ncol;
+    printf("¬ведите количество строк и столбцов массива\n");
+    scanf("%d%d",&nrow, &ncol);
+    int **arr = (int**)malloc(nrow * sizeof(int*));
+    if(arr == NULL)
+    {
+        printf("ќшибка выделени¤ пам¤ти\n");
+        exit(1);
+    }
+    for(int i = 0; i < nrow; i++)
+        arr[i] = (int*)malloc(ncol * sizeof(int));
+    for(int i = 0; i < nrow; i++)
+    {
+        if(arr[i] == NULL)
+        {
+            printf("ќшибка выделени¤ пам¤ти\n");
+            exit(1);
+        }
+    }
+
+    for(int i = 0; i < nrow; i++)
+        for(int j = 0; j < ncol; j++)
+            arr[i][j] = rand()%100 - 50;
+    printf("Ёлементы массива равны:\n");
+    for(int i = 0; i < nrow; i++)
+    {
+        for(int j = 0; j < ncol; j++)
+                printf("%4d ", arr[i][j]);
+        printf("\n");
+    }
+    int *sum = (int*)malloc(nrow * sizeof(int));
+    if(sum == NULL)
+    {
+        printf("ќшибка выделени¤ пам¤ти\n");
+        exit(1);
+    }
+    for(int i = 0; i < nrow; i++)
+    {
+        sum[i] = 0;
+        for(int j = 0; j < ncol; j++)
+            sum[i] += arr[i][j];
+    }
+    printf("—уммы строк массива:\n");
+    for(int i = 0; i < nrow; i++)
+        printf("%4d ", sum[i]);
+    long buf_sum;
+    int nmin, buf_a;
+    for(int i = 0; i < nrow - 1; i++)
+    {
+        nmin = i;
+        for(int j = i + 1; j < nrow; j++)
+            if(sum[j] < sum[nmin]) nmin = j;
+        buf_sum = sum[i];
+        sum[i] = sum[nmin];
+        sum[nmin] = buf_sum;
+        for(int j = 0; j < ncol; j++)
+        {
+            buf_a = arr[i][j];
+            arr[i][j] = arr[nmin][j];
+            arr[nmin][j] = buf_a;
+        }
+    }
+    printf("\nЁлементы упор¤доченного массива:\n");
+    for(int i = 0; i < nrow; i++)
+    {
+        for(int j = 0; j < ncol; j++)
+                printf("%4d ", arr[i][j]);
+        printf("\n");
+    }
+    printf("—уммы строк упор¤доченного массива:\n");
+    for(int i = 0; i < nrow; i++)
+        printf("%4d ", sum[i]);
+    for(int i = 0; i < nrow; i++)
+        free(arr[i]);
+    free(arr);
+    return 0;
+}

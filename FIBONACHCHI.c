3ad330617df5990c7ef873865fc333c8b2947db9
SirEagIe <57668483+SirEagIe@users.noreply.github.com>
2020-05-23 16:21:51 +1000

    Create FIBONACHCHI.c

---
+++ FIBONACHCHI.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    int n, f1 = 0, f2 = 1, f3;
+    printf("Enter n\n");
+    scanf("%d", &n);    //ввод номера члена последовательности
+    if(n == 1)      //если 1, выводим первый член
+        printf("%d", f1);
+    else if(n == 2) //если 2, выводим второй член
+        printf("%d", f2);
+    else    //иначе считаем
+    {
+        for(int i = 0; i < n - 2; i++)  //цикл с n-2 итерациями (n-2, т. к. 2 элемента уже есть)
+        {
+            f3 = f1 + f2;   //тут сам рпзберись :P
+            f1 = f2;
+            f2 = f3;
+        }
+        printf("%d", f3);
+    }
+    return 0;
+}
